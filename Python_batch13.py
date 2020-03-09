d6ddb5a53e6c6c3ccd8c1199fd30c2fef0b7cd13
shazia90 <61934410+shazia90@users.noreply.github.com>
2020-03-09 21:48:47 +0530

    Add files via upload

---
+++ Python_batch13.py
@@ -0,0 +1,376 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# variables
+
+
+# In[2]:
+
+
+x=5
+y=4
+
+
+# In[3]:
+
+
+x+y
+
+
+# In[4]:
+
+
+first name='r'
+last name='anupriya'
+
+
+# In[5]:
+
+
+first_name='r'
+
+
+# In[6]:
+
+
+last_name='anupriya'
+
+
+# In[7]:
+
+
+1name='test'
+
+
+# In[8]:
+
+
+# int the begining we cant use no
+
+
+# In[9]:
+
+
+name1='test'
+
+
+# In[10]:
+
+
+name2='test'
+
+
+# In[11]:
+
+
+# we cant use any special characteers while declaring the variables
+
+
+# In[12]:
+
+
+# Introduction to String data types
+
+
+# In[13]:
+
+
+#Possible ways  for defining a String
+
+#''------------->Single quotes
+#""------------->Double quotes
+
+
+# In[14]:
+
+
+first_name="marry"
+
+
+# In[15]:
+
+
+print(first_name)
+
+
+# In[16]:
+
+
+last_name='jane'
+
+
+# In[17]:
+
+
+print(last_name)
+
+
+# In[18]:
+
+
+name="""sani"""
+
+
+# In[19]:
+
+
+print(name)
+
+
+# In[21]:
+
+
+#Mutable data type & immutable data type
+
+# Mutable data type: The data type which allowing us to modify, cahnge and reassign once it has been created
+
+# Immutable data type: The data type which will be not allowing us to modify, change or alter once it gets created.
+
+# String is considered as an immutable data type.
+
+
+# In[22]:
+
+
+# Introduction to String methods
+
+# Built in methods
+
+
+# In[23]:
+
+
+name= "praveen murti"
+
+
+# In[24]:
+
+
+print(name)
+
+
+# In[26]:
+
+
+# Enhancement of code
+
+print(name.title()) # converts the first letter to capital
+
+
+# In[27]:
+
+
+#req: all the letters need to convert into capitals
+
+print(name.upper())
+
+
+# In[28]:
+
+
+# req:  all the letters need to convert into small case
+
+print(name.lower())
+
+
+# In[29]:
+
+
+first_name='tony'
+
+
+# In[30]:
+
+
+last_name='stark'
+
+
+# In[31]:
+
+
+full_name=f'{first_name}{last_name}'
+
+
+# In[32]:
+
+
+print('{} is your name'.format(full_name) )
+
+
+# In[33]:
+
+
+# adding whitespaces to string
+
+print(' python')
+
+
+# In[34]:
+
+
+print('python  ')
+
+
+# In[36]:
+
+
+print('\tpython')
+
+
+# In[37]:
+
+
+print('languages:\nPython\njava\nswift\nc\nc++')
+
+
+# In[38]:
+
+
+#Enhancement of code
+
+print('languages:\n\tPython\n\tjava\n\tswift\n\tc\n\tc++')
+
+
+# In[39]:
+
+
+# How to eliminate sapces
+
+fav_lang='  python' #left side
+
+print(fav_lang)
+
+
+# In[41]:
+
+
+fav_lang2='java  '#right side
+
+print(fav_lang2)
+
+
+# In[42]:
+
+
+fav_lang.lstrip()
+
+
+# In[43]:
+
+
+fav_lang2.rstrip()
+
+
+# In[44]:
+
+
+fav_lang3='   swift   '
+print(fav_lang3)
+
+
+# In[46]:
+
+
+fav_lang3.strip() #this will check both sides
+
+
+# In[47]:
+
+
+# Introduction to operations in no
+
+3+2
+
+
+# In[48]:
+
+
+4*3
+
+
+# In[49]:
+
+
+5-2
+
+
+# In[50]:
+
+
+3/2
+
+
+# In[51]:
+
+
+#How do you square in python
+
+4**2
+
+
+# In[52]:
+
+
+3**4
+
+
+# In[53]:
+
+
+4**3
+
+
+# In[54]:
+
+
+#Decimals
+
+3.1+4.2
+
+
+# In[55]:
+
+
+#How to verify data type
+
+x=1.5
+
+
+# In[56]:
+
+
+y=2.5
+
+
+# In[57]:
+
+
+z=x+y
+
+
+# In[58]:
+
+
+print(z)
+
+
+# In[59]:
+
+
+type(z)
+
+
+# In[60]:
+
+
+type(name)
+
+
+# In[ ]:
+
+
+
+
